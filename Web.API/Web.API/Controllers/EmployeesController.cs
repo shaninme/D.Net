843bbddb5c4d0dc46ddc40d5b5646e0b8d4eee03
sreenarajk <sreenu.raj@gmail.com>
2019-09-14 00:38:15 +0530

    Initial project files commit

---
+++ Web.API/Web.API/Controllers/EmployeesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Data.Entities;
+
+namespace Web.API.Controllers
+{
+    [Authorize]
+    public class EmployeesController : ApiController
+    {
+        private SkyEntities entities = new SkyEntities();
+        public IEnumerable<Employee> Get()
+        {
+            using (entities)
+            {
+                return entities.Employees.ToList();
+            }
+        }
+    }
+}
